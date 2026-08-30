27f6565513e834c972830b856af702a0d6610cd8
mto-commits <mikkojalehto@gmail.com>
2026-08-30 15:23:51 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE='kp-galaxy-ultra-v4-4-predictive';
+const CACHE='kp-galaxy-ultra-v4-5-world-sniper';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(()=>self.skipWaiting()));
