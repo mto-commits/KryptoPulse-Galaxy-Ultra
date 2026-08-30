97542cda1826a534532cc170a225784521880d4a
mto-commits <mikkojalehto@gmail.com>
2026-08-30 17:49:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,5 @@
-
-const CACHE='kp-galaxy-ultra-v5.3-microcap-rocket-hunter-pro';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener('fetch', event => {
-  if(event.request.method!=='GET') return;
-  const url=new URL(event.request.url);
-  if(url.origin===self.location.origin){
-    event.respondWith(fetch(event.request).then(resp=>{
-      const copy=resp.clone();
-      caches.open(CACHE).then(c=>c.put(event.request,copy));
-      return resp;
-    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
-  }
-});
+const CACHE='kryptopulse-galaxy-v5-4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
