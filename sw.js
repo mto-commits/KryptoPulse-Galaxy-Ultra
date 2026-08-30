bc9e7d0f9fce3425b85dea0ff4245aa5b146d7c5
mto-commits <mikkojalehto@gmail.com>
2026-08-30 23:05:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kryptopulse-galaxy-v7-4-1-clean-20260830-2253';
+const CACHE='kryptopulse-galaxy-v7-5-ai-discovery-20260830';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
