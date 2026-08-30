ee90774e3db33792855809904f7520337b922b32
mto-commits <mikkojalehto@gmail.com>
2026-08-30 23:54:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kryptopulse-galaxy-v8-1-canonical-bus-20260830';
+const CACHE='kryptopulse-galaxy-v8-2-position-truth-20260830';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
