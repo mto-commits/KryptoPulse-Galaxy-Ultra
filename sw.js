e5f54179aff0081835cc3cf92fc6bbd9d470153f
mto-commits <mikkojalehto@gmail.com>
2026-08-30 13:43:47 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,20 @@
 
-const CACHE='kp-galaxy-ultra-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-      const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
-    }).catch(()=>caches.match('./index.html'))));
+const CACHE='kp-galaxy-ultra-v2';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  if(url.origin===self.location.origin){
+    event.respondWith(fetch(event.request).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(c=>c.put(event.request,copy));
+      return resp;
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
   }
 });
